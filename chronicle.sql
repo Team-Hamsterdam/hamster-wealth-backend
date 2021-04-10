a35cd641be04c2fe643e51bebb05b12124634c19
Matthew Lau <matthew.lau78@gmail.com>
2021-04-11 04:31:13 +1000

    email removed

--- chronicle.sql
+++ chronicle.sql
@@ -2,9 +2,8 @@
 CREATE TABLE IF NOT EXISTS client (
     token text unique not null,
     username text not null,
-    email text not null,
     password text not null,
-    primary kEY (token)
+    primary key (token)
 );
 CREATE TABLE IF NOT EXISTS portfolio (
     token text not null,
@@ -22,10 +21,3 @@ CREATE TABLE IF NOT EXISTS stock (
     units int,
     primary key (portfolio_id, ticker)
 );
--- UPDATE portfolio
---                 SET  balance = {balance[0] + cash_amt}
---                 WHERE token = "{parsed_token}" and portfolio_id = {portfolio_id};
--- UPDATE user_table
---     set logged_in = 1
---     where user_table.token = '{token}';
--- select portfolio_id, title, balance from portfolio where token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImUxYWMzOWI2Y2NlZGEzM2NjOGNhNDNlOWNiYzE0ZjY2ZmFiODVhNGMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiOTEyNTczNTYzNTU4LWdpbTAwb28wZDVmMzR1aTdtNzhqMXEydmxkaXZxcnZkLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiOTEyNTczNTYzNTU4LWdpbTAwb28wZDVmMzR1aTdtNzhqMXEydmxkaXZxcnZkLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTEwMTcxNjM0MjEzMDkxMzcxOTYwIiwiZW1haWwiOiJqb25ueXNtaXRoMjE2MjE2QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiNEMwcFNVcnFWbkxjRUg4TG92cjhFdyIsIm5vbmNlIjoiZ0VMT0RMMGtHcXBMWEZ4TDFyTjYiLCJpYXQiOjE2MTgwMzMxMTMsImV4cCI6MTYxODAzNjcxM30.Pw4hYQrIa39locbdfv1neG00wROaSEmAJyl2JOk1k4xomzGe3QAwEDqTmyuMCC4CoYMGpSJi9ArfSEu_npfefoG9gwiqL6LZy8p-ddWUf3U1IltO-CHkG0vsfQI2NX2hmxbZ_6lXcd3egTRTOZ4Vg5usz7cRkuWjKBMSORNshveJP4ohocq-JxMqTMJvqGCr4RTFe5JvXTzo-Hc560U5HEP1Pc7RylUiz1FgNXuNV8-ttEbL7g3_5A1uVpGSWtKdgaHtQ5PV2QFt0h3D7E-yCGYhjSH2jCk2irWQetf2cYIZNKI1GCt0PMzM-2oSXvv_wrr0OWwc6exXRvhifxS0ow';
